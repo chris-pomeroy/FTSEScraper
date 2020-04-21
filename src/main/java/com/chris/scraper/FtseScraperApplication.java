1257e672cbb1b5881af5993d00856f3d32ccd2fa
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2020-04-21 15:45:02 +0100

    Added dependencies

---
+++ src/main/java/com/chris/scraper/FtseScraperApplication.java
@@ -0,0 +1,13 @@
+package com.chris.scraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtseScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtseScraperApplication.class, args);
+	}
+
+}
