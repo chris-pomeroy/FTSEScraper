6c71f1d38bdb479e317ce9393ce1d21b7fa2e72b
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2020-04-21 20:23:59 +0100

    Removed import

--- src/main/java/com/chris/scraper/FtseScraperApplication.java
+++ src/main/java/com/chris/scraper/FtseScraperApplication.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 public class FtseScraperApplication {
