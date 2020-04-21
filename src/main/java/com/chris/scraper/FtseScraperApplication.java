6440793193cd22d2717308b62de67a6508f4af22
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2020-04-21 16:41:00 +0100

    Project setup working

--- src/main/java/com/chris/scraper/FtseScraperApplication.java
+++ src/main/java/com/chris/scraper/FtseScraperApplication.java
@@ -1,9 +1,15 @@
 package com.chris.scraper;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 public class FtseScraperApplication {
 
 	public static void main(String[] args) {
 		
+		RestTemplate rest = new RestTemplate();
+		ResponseEntity<String> entity = rest.getForEntity("http://www.google.com", String.class);
+		System.out.println(entity.getBody());
 	}
-
+	
 }
