30a91fabfe972848b56c23d37605b7b1a3108967
Chris Pomeroy <chris.pomeroy@hotmail.co.uk>
2020-04-21 19:03:20 +0100

    Working for one stock

--- src/main/java/com/chris/scraper/FtseScraperApplication.java
+++ src/main/java/com/chris/scraper/FtseScraperApplication.java
@@ -8,8 +8,10 @@ public class FtseScraperApplication {
 	public static void main(String[] args) {
 		
 		RestTemplate rest = new RestTemplate();
-		ResponseEntity<String> entity = rest.getForEntity("http://www.google.com", String.class);
-		System.out.println(entity.getBody());
+		ResponseEntity<String> entity = rest.getForEntity("https://query1.finance.yahoo.com/v7/finance/download/BARC.L?period1=1555804800&period2=1587427200&interval=1wk&events=history", String.class);
+		
+		System.out.println(new StockInfo("BARC.L", entity.getBody()));
+		
 	}
 	
 }
